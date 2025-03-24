dde4cb851e3b45599c31615e06c70e80eedf2b82
markWangsgard <mark.wangsgard@students.snow.edu>
2025-03-24 12:45:48 -0600

    Add User Method Created

--- SocialNetworkLogic/SocialNetwork.cs
+++ SocialNetworkLogic/SocialNetwork.cs
@@ -2,5 +2,22 @@
 
 public class SocialNetwork
 {
-    private Dictionary<string, List<string>> friendLists = new Dictionary<string, List<string>>; 
+    private Dictionary<string, List<string>> friendLists;
+
+    public SocialNetwork()
+    {
+        friendLists = new Dictionary<string, List<string>>();
+    }
+
+    public void AddUser(string name)
+    {
+        if (friendLists.ContainsKey(name))
+        {
+            Console.WriteLine($"{name} already exists.");
+            return;
+        }
+
+        friendLists[name] = new List<string>();
+        Console.WriteLine($"{name} has been added.");
+    }
 }
