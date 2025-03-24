668f4a2b8369b56b7f0e0f05190c924f48044245
markWangsgard <mark.wangsgard@students.snow.edu>
2025-03-24 13:14:45 -0600

    AddFriend Method Added

--- SocialNetworkLogic/SocialNetwork.cs
+++ SocialNetworkLogic/SocialNetwork.cs
@@ -2,44 +2,72 @@
 
 public class SocialNetwork
 {
-    private Dictionary<string, List<string>> friendLists;
+    private Dictionary<string, List<string>> socialNetworkDictionary;
 
     public SocialNetwork()
     {
-        friendLists = new Dictionary<string, List<string>>();
+        socialNetworkDictionary = new Dictionary<string, List<string>>();
     }
 
     public void AddUser(string name)
     {
-        if (friendLists.ContainsKey(name))
+        if (socialNetworkDictionary.ContainsKey(name))
         {
             Console.WriteLine($"{name} already exists.");
             return;
         }
 
-        friendLists[name] = new List<string>();
+        socialNetworkDictionary[name] = new List<string>();
         Console.WriteLine($"{name} has been added.");
     }
 
     public Dictionary<string, List<string>> GetNetwork()
     {
-        return friendLists;
+        return socialNetworkDictionary;
     }
 
     public void RemoveUser(string name)
     {
-        if (!friendLists.ContainsKey(name))
+        if (!socialNetworkDictionary.ContainsKey(name))
         {
             Console.WriteLine($"{name} does not exist.");
             return;
         }
 
-        var listOfFriends = friendLists[name];
+        var listOfFriends = socialNetworkDictionary[name];
         foreach(var friend in listOfFriends)
         {
-            friendLists[friend].Remove(name);
+            socialNetworkDictionary[friend].Remove(name);
         }
-        friendLists.Remove(name);
+        socialNetworkDictionary.Remove(name);
         Console.WriteLine($"{name} has been removed from the network.");
+    }
+
+    public void AddFriend(string user1, string user2)
+    {
+        if (!twoUsersExists(user1, user2))
+        {
+            Console.WriteLine("One or both users do not exist.");
+            return;
+        }
+        if (socialNetworkDictionary[user1].Contains(user2))
+        {
+            Console.WriteLine($"{user1} and {user2} are already friends.");
+            return;
+        }
+        socialNetworkDictionary[user1].Add(user2);
+        socialNetworkDictionary[user2].Add(user1);
+        Console.WriteLine($"{user1} and {user2} are now friends.");
+    }
+
+
+
+    private bool userExists(string user)
+    {
+        return socialNetworkDictionary.ContainsKey(user);
+    }
+    private bool twoUsersExists(string user1, string user2)
+    {
+        return socialNetworkDictionary.ContainsKey(user1) && socialNetworkDictionary.ContainsKey(user2);
     }
 }
